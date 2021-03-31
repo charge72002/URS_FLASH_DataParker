bfc428cae9baf84d88e3309dc4ccd673a2eb4d8c
Sherry Wong <bwong24@wisc.edu>
2021-03-30 20:35:23 -0500

    Made colorbar consistent. Added streamline annotation capabilities.

--- PlotEverything.py
+++ PlotEverything.py
@@ -19,7 +19,7 @@ startTime = time.time()
 
 # # how to get a fixed colorbar???
 #slice plotting
-field = "temp"
+field = "density"
 for fileName in os.listdir(directory):
     if(fileName.startswith("parkerCRs")):
         #Start
@@ -32,12 +32,16 @@ for fileName in os.listdir(directory):
         # slc.annotate_velocity(factor=16)
         slc.annotate_title(timeStamp +" "+ field)
         plot = slc.plots[field]
-        plot.cb.set_ticks([1e3, 1e4, 1e5])
-        plot.cb.set_ticklabels(["$10^3$", "$10^4$", "$10^5$"])
-        
-        #Finish
-        slc.save(saveDirectory + "/" + field + "/" + timeStamp)
+        # plot.cb.set_ticks([1e3, 1e4, 1e5])
+        # plot.cb.set_ticklabels(["$10^3$", "$10^4$", "$10^5$"])
+        # slc.set_zlim('temp', 1e2, 1e6)
+        slc.annotate_streamlines('magnetic_field_x','magnetic_field_y',density=3,plot_args={'linewidth':0.5,'color':'r'}) 
 
+        #Finish
+        #https://stackabuse.com/creating-and-deleting-directories-with-python/
+        if (not path.exists(saveDirectory + "/" + field)):
+            os.mkdir(saveDirectory + "/" + field)
+        slc.save(saveDirectory + "/" + field + "/" + timeStamp)
 print()
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print("Plotting done. Time elapsed (sec): " + (time.time()-startTime))
@@ -51,3 +55,12 @@ plot.cb.set_ticks([1e3, 1e4, 1e5])
 plot.cb.set_ticklabels(["$10^3$", "$10^4$", "$10^5$"])
 slc.save("YT_Test_Plots/HDF5/ColorbarTest")
 # slc.save(saveDirectory + "/" + field + "/" + timeStamp)
+
+mass = []
+# Cell Mass
+for fileName in os.listdir(directory):
+    if(fileName.startswith("parkerCRs")):
+        #Start
+        print(fileName)
+        ds = yt.load(directory+fileName)
+        print(sum(ds[('gas', 'cell_mass')]))
--- StreamVectorPlottingTest.py
+++ StreamVectorPlottingTest.py
@@ -5,40 +5,48 @@ Created on Thu Jan 14 22:37:53 2021
 @author: Sherry Wong
 """
 
-import h5py #installed with "pip install h5py"
-import matplotlib
-import matplotlib.pyplot as plt #Same installation as above
-from matplotlib import ticker, cm #for log scale
-import matplotlib.colors as colors #for color mapping
-# import matplotlib.colors.LogNorm #for logorithmic color mapping
 import numpy as np
-import sys
-import csv #https://docs.python.org/3/library/csv.html#csv.Dialect
-from yt.visualization.api import Streamlines
-
 import yt
 from yt.units import kpc
+import os
 
+os.chdir("/Users/wongb/Documents/Python Scripts")
 #filename = "/Users/wongb/Documents/URS Data/m1.5_c1_16x16_128x128_Rodrigues_Streaming/More Plot Files/parkerCRs_hdf5_plt_cnt_0020"
 filename = "/Users/wongb/Documents/URS Data/m2_c1_16x8_64x64/More Plot Files/parkerCRs_hdf5_plt_cnt_0076"
+filename = "/Users/wongb/Documents/URS Data/diffusion_3e28/diffusion_3e28/parkerCRs_hdf5_plt_cnt_0000"
 
 #see https://yt-project.org/doc/analyzing/objects.html
 ds = yt.load(filename)
 
+for i in sorted(ds.field_list):
+    print(i)
+for i in sorted(ds.derived_field_list):
+    print(i)
+print((('flash', 'cray')) == ('flash', 'cray'));
+
+#####################################
+# SlicePlot + Vectors + Streamlines #
+#####################################
+
 #Zoom
 slc = yt.SlicePlot(ds, 'z', 'density', center=(4.1*kpc, -2.85*kpc, 0), width=(2*kpc, 2*kpc, 0)) #3D!!!
-slc.annotate_velocity(factor=16)
+# slc.annotate_velocity(factor=16)
 slc.zoom(10);
-slc.set_width(.2*kpc)
+slc.set_width(2*kpc)
 # slc.set_unit('temp', 'K')
-slc.annotate_title("parkerCRs_hdf5_plt_cnt_0076 Density + Velocity")
-slc.save("YT_Test_Plots/HDF5/VectorZoom4");
+slc.annotate_title("parkerCRs_hdf5_plt_cnt_0076 Density + Magnetic Field")
+slc.annotate_streamlines('magnetic_field_x','magnetic_field_y',density=3,plot_args={'linewidth':0.5,'color':'r'}) 
+# slc.annotate_streamlines('velocity_x','velocity_y',density=3,plot_args={'linewidth':0.5,'color':'r'}) 
+slc.save("YT_Test_Plots/HDF5/StreamMagZoom3");
+
 
 #No zoom
 slc = yt.SlicePlot(ds, 'z', 'density')
-slc.annotate_velocity(factor=16)
+# slc.annotate_velocity(factor=16)
 slc.annotate_title("parkerCRs_hdf5_plt_cnt_0076 Density + Velocity")
-slc.save("YT_Test_Plots/HDF5/Vector2")
+# slc.annotate_streamlines('magnetic_field_x','magnetic_field_y',density=2,plot_args={'linewidth':0.5,'color':'r'}) 
+slc.annotate_streamlines('velocity_x','velocity_y',density=4,plot_args={'linewidth':0.5,'color':'r'}) 
+slc.save("YT_Test_Plots/HDF5/StreamVel2")
 
 # slc.hide_axes()
 # velMagnitude = np.hypot(ds.r['velx'], ds.r['vely'])
@@ -48,11 +56,7 @@ slc.save("YT_Test_Plots/HDF5/Vector2")
 #     return np.hypot(data['velx'], data['vely'])
 # ds.add_field((ds, "velMagnitude"), units="cm/s", function=velMagnitude)
 # 
-for i in sorted(ds.field_list):
-    print(i)
-for i in sorted(ds.derived_field_list):
-    print(i)
-print((('flash', 'cray')) == ('flash', 'cray'));
+
 # =============================================================================
 
 ##################
