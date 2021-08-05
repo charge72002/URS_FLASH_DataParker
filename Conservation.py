8ed00c9c1b15ccfef0cc568258e2bf72e568723d
Brandon Wong <bwong@davids-MacBook-Air-2.local>
2021-08-05 16:19:54 -0400

    Implemented energy calculations. CHECK VALIDITY.

--- Conservation.py
+++ Conservation.py
@@ -10,13 +10,14 @@ filename = "/Users/wongb/Documents/URS Data/m2_c1_16x8_64x64/More Plot Files/par
 #no slash at end
 filedirectory = "/Users/wongb/Documents/URS Data/m2_c1_16x8_64x64/More Plot Files"
 pwd = "/Users/wongb/Documents/Python_Scripts"
-#%%
+#%%s
 ## Mac filenames
 filename = "/Users/bwong/Downloads/URS_Data/m2_c1_16x8_64x64/More Plot Files/parkerCRs_hdf5_plt_cnt_0080"
 filedirectory = "/Users/bwong/Downloads/URS_Data/m2_c1_16x8_64x64/More Plot Files"
 pwd = "/Users/bwong/URS_FLASH_DataParker"
 #%%
 import yt
+from yt.units import *
 import os
 import os.path
 from os import path
@@ -34,6 +35,65 @@ sys.path.insert(0, pwd)
 import estimate_bubbleVelocity as est
 import hdf5_parser
 
+#mess with classes
+"""The energies class calculates simulation energy in ergs."""
+class energies:
+    #CONSTANTS
+    #C= -2pi * newton's constant G * surface density \Sigma
+    C = (-2 * math.pi) * (6.6743 * 10**-8) * (9.84 * 10**-3)
+    H = 7.715 * 10**20
+    
+    ad, KE, PE_grav, PE_mag, PE_cr, PE_thermo = None, None, None, None, None, None
+    """class constructor using yt dataset ds"""
+    def __init__(self, ds, gamma_cr = 4/3, gamma_gas = 5/3, C = C, H=H):
+        startTime = time.time()
+        # C = self.C
+        # H = self.H
+        # gamma_cr = 4/3
+        # gamma_gas = 5/3
+        ad = ds.all_data()
+        
+        #energy densities in dynes/cm^2 aka erg/cm^3
+        KE = ad['kinetic_energy']
+        magEnergy = ad[('gas', 'magnetic_energy')]
+        pressure = ad[('gas', 'pressure')]
+        #CR energy density in erg/g
+        cray = ad['flash', 'cray'] * (erg/g)
+        
+        volume = ad[('gas', 'cell_volume')]
+        mass = ad[('gas', 'cell_mass')]
+        #gravitational potential in erg/g
+        #use numpy instead of math package on array
+        phi = C*H*np.log(np.cosh(ad['y']/H)) * erg/g
+        
+        #turn everything into ergs
+        self.KE = KE * volume
+        self.PE_grav = phi * mass
+        # self.PE_mag =  ((magField**2) / (8*math.pi)) * volume #using Roark mag field in gauss
+        self.PE_mag = magEnergy * volume
+        # self.PE_cr = cray * (1/(gamma_cr-1)) * m #using Roark cray pres
+        self.PE_cr = cray * mass
+        self.PE_thermo = pressure * (1/(ds.gamma-1)) * volume
+        print("energies.totals() done. Time elapsed (sec): " + str(time.time()-startTime))
+
+        
+    """Returns a dictionary with field totals, including the total energy combined"""
+    def totals(self):
+        startTime = time.time()
+        totals = {'total':0,
+                  'KE': sum(self.KE), 
+                  'PE_grav': sum(self.PE_grav), 
+                  'PE_mag': sum(self.PE_mag), 
+                  'PE_cr': sum(self.PE_cr), 
+                  'PE_thermo': sum(self.PE_thermo)}
+        for field in totals.keys(): 
+            # print(field)
+            totals['total'] += totals[field]
+            # if not(field=='total'): totals['total'] += totals[field]
+        print(totals.keys())
+        print("energies.totals() done. Time elapsed (sec): " + str(time.time()-startTime))
+        return totals
+
 # # directory = "/Users/wongb/Documents/URS Data/m2_c1_16x8_64x64/More Plot Files/"
 # # directory = "D://URS_LargeData/Parker_forSherry/"
 directory = "D://URS_LargeData/m2_c1_16x8_64x64_cfl06_tx10/"
@@ -44,20 +104,31 @@ saveDirectory = "D:/URS_LargeData/SherryPlots"
 # path.exists(saveDirectory)
 
 #%%
-ds = yt.load("D://URS_LargeData/Parker_forSherry/parkerCRs_hdf5_plt_cnt_0700")
+ds = yt.load(filename)
 ad = ds.all_data()
+
+#%%
+### test energies class
+eng = energies(ds)
+totals = eng.totals()
+for field in totals.keys(): print(str(field) + ": " + str(totals[field]))
 
 #%%
 # print(ds.field_list)
 # print(ds.derived_field_list)
 
-print(ad[('gas', 'kinetic_energy')][0])
+print(ad[('gas', 'kinetic_energy')][0] * ad[('gas', 'cell_volume')][0])
 m = ad[('gas', 'cell_mass')][0]
+# vol = ad[('gas', 'cell_volume')][0]
 v = ad[('gas', 'velocity_magnitude')][0]
 print(0.5 * m * (v**2))
+# print(vol * 0.5 * m * (v**2))
+
+#Thermal energy NOT INCLUDED in KE; must track separately
+#%%
 
 print("\nTOTAL energies:")
-print(sum(ad[('gas', 'kinetic_energy')]))
+print(sum(ad[('gas', 'kinetic_energy')])* ad[('gas', 'cell_volume')][0])
 # m = sum(ad[('gas', 'cell_mass')]) #in g
 # v = sum(ad[('gas', 'velocity_magnitude')]) #in cm/s
 # print(0.5 * m * (v**2))
@@ -67,6 +138,7 @@ print(sum(0.5 * m * (v**2)))
 
 print('\n')
 print("Units: " + str(ds.mass_unit) + ", " + str(ds.length_unit) + ", " + str(ds.temperature_unit))
+print('cray units in ergs/g')
 #%%
 
 #####################
