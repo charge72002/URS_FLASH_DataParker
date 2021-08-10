3e865671189b7a93a2e47856479faea6f3e43695
charge72002 <bwong24@wisc.edu>
2021-08-10 15:51:24 -0400

    2D threshold exercises

--- 2D_MagMagnitude.py
+++ 2D_MagMagnitude.py
@@ -20,6 +20,7 @@ pwd = "/Users/bwong/URS_FLASH_DataParker"
 import yt
 import matplotlib.pyplot as plt
 from yt.units import cm
+yt.toggle_interactivity()
 
 import numpy as np
 
@@ -49,38 +50,68 @@ plt.plot([0, len(ad[('gas', 'magnetic_field_magnitude')])], [0, 0], lw=1) #lines
 
 #%%
 # plot bubble positions on yt slice
-TFselect = ad[('gas', 'magnetic_field_magnitude')] < 1.25E-9 #using this threshold
-print(str(sum(TFselect)) + " matches")
-
-xbubblepos = ad['x'][TFselect]
-ybubblepos = ad['y'][TFselect]
-
-slc = yt.SlicePlot(ds, 'z', 'magnetic_field_magnitude')
-# slc = yt.SlicePlot(ds, 'z', 'density')
-slc.annotate_streamlines('magnetic_field_x','magnetic_field_y', density=6, plot_args={'linewidth':0.25,'color':'r'})
-length = 1E21*cm
-#plot '+' on percieved bubble positions
-for i in range(0, len(xbubblepos)):
-    x = xbubblepos[i]
-    y = ybubblepos[i]
-    slc.annotate_line((x, y+length, 0), (x, y-length, 0), coord_system="data",  plot_args={"color": "red", "linewidth": 1})
-    slc.annotate_line((x+length, y, 0), (x-length, y, 0), coord_system="data",  plot_args={"color": "red", "linewidth": 1})
-slc.save(pwd + "/bubble_velocity_2D/MagMagnitude")
+# iteratively test different thresholds
+# for threshold in np.linspace(1.0, 7.5, 14):
+for threshold in np.linspace(5.0, 10.0, 11):
+    TFselect = ad[('gas', 'magnetic_field_magnitude')] < threshold*(10**-9) #using this threshold
+    print(str(sum(TFselect)) + " matches")
+    
+    xbubblepos = ad['x'][TFselect]
+    ybubblepos = ad['y'][TFselect]
+    
+    slc = yt.SlicePlot(ds, 'z', 'magnetic_field_magnitude')
+    # slc = yt.SlicePlot(ds, 'z', 'density')
+    slc.annotate_streamlines('magnetic_field_x','magnetic_field_y', density=6, plot_args={'linewidth':0.25,'color':'black'})
+    length = 1E21*cm
+    #plot '+' on percieved bubble positions
+    for i in range(0, len(xbubblepos)):
+        x = xbubblepos[i]
+        y = ybubblepos[i]
+        slc.annotate_line((x, y+length, 0), (x, y-length, 0), coord_system="data",  plot_args={"color": "red", "linewidth": 1})
+        slc.annotate_line((x+length, y, 0), (x-length, y, 0), coord_system="data",  plot_args={"color": "red", "linewidth": 1})
+    slc.save(pwd + "/bubble_velocity_2D/" + str(threshold))
 
 #%%
+## visualize threshold with yt.cut_region
+for threshold in np.linspace(1.0, 10.0, 19):
+    dsSelect = ad.cut_region("obj['magnetic_field_magnitude'] < " + str(threshold) + "e-9")
+    slc = yt.SlicePlot(ds, 'z', 'magnetic_field_x', data_source=dsSelect)
+    # slc.annotate_streamlines('magnetic_field_x','magnetic_field_y', density=6, plot_args={'linewidth':0.25,'color':'black'})
+    slc.save(pwd + "/bubble_velocity_2D/dsSelect" + str(threshold))
+
+#%%
+## what does the magnetic field strength even look like
+slc = yt.SlicePlot(ds, 'z', 'magnetic_field_magnitude')
+slc.show_colorbar()
+slc.save(pwd + "/bubble_velocity_2D/mag")
+
+slc = yt.SlicePlot(ds, 'z', 'magnetic_field_x')
+slc.show_colorbar()
+slc.save(pwd + "/bubble_velocity_2D/magX00")
+
+slc = yt.SlicePlot(ds, 'z', 'magnetic_field_y')
+slc.show_colorbar()
+slc.save(pwd + "/bubble_velocity_2D/magY00")
+
+    
+ #%%
 # iterative implementation of threshold
-TFselect = ad[('gas', 'magnetic_field_magnitude')] < 1.25E-9 #using this threshold
+TFselect = ad[('gas', 'magnetic_field_magnitude')] < 4E-9 #using this threshold
 print(str(sum(TFselect)) + " matches")
     
 slc = yt.SlicePlot(ds, 'z', 'magnetic_field_magnitude')
 slc.annotate_streamlines('magnetic_field_x','magnetic_field_y', density=6, plot_args={'linewidth':0.25,'color':'r'})
 length = 1E21*cm
+num_matches=0
 for i in range(0, len(TFselect)):
     if TFselect[i]:
     #select based on mag slope
-        avgSlope = 0.5*(ad['magx'][i-1]-ad['magx'][i+1])
+        avgSlope = 0.5*(ad['magx'][i-1]-ad['magx'][i+1]) #broken. selects Z order neighbors which means nothing
         if avgSlope > 5*(10**-11):
+            num_matches+=1
             slc.annotate_line((x, y+length, 0), (x, y-length, 0), coord_system="data",  plot_args={"color": "red", "linewidth": 1})
             slc.annotate_line((x+length, y, 0), (x-length, y, 0), coord_system="data",  plot_args={"color": "red", "linewidth": 1})
+print(str(sum(TFselect)) + " matches")
+print("Reduced to " + str(num_matches) + " matches\n")
 slc.save(pwd + "/bubble_velocity_2D/MagMagnitudeTrim")
    
