01301e8739841367d57a2c94f2ccead972d3d2df
charge72002 <bwong24@wisc.edu>
2021-08-12 16:45:08 -0400

    fixed algebra mistake

--- estimate_bubbleVelocity.py
+++ estimate_bubbleVelocity.py
@@ -16,23 +16,24 @@ H = 7.715 * 10**20
 def calcGravity(y):
     return C * math.tanh(y/H)
 
-# TODO: implement additional gravity within 1kpc (3E21cm) of galactic disk
 # returns estimated ballistic velocity in cm/s
+# y is some number, y0 and v0 are previous known values for y<4H.
+# TODO: implement this to iterate over a list of y values?
 def calcVelocity(y, y0=0, v0=0):
     #gravity becomes constant after 4 scale heights
     if(abs(y)<(4*H)): #y<1~kpc for Solar Neighborhood Parameters
         print("Inside the disk 2")
         b = 2*C*H*math.log(math.cosh(y0/H))
         c = 2*C*H*math.log(math.cosh(y/H))
-        X = v0**2 - b + c
+        X = 0.5v0**2 - b + c
         # print(X)
         #resolve sign issue
-        if(X>0): return math.sqrt(X)
-        else: return -(math.sqrt(-X))
+        if(X>0): return 2*math.sqrt(X)
+        else: return -2*(math.sqrt(-X))
     else:
         # g = calcGravity(y) 
-        g = C #Constant gravity; tanh(y/H)~1
-        v = math.sqrt( abs(2*y*g) )
+        #Constant gravity; tanh(y/H)~1
+        v = math.sqrt( abs(2*y*C) )
         if(g<0): v*=-1;
         return v
 
