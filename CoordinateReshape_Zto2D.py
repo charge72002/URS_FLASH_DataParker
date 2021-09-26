710f74f01aebd40f40828f7f16c760ad2f1236e7
charge72002 <bwong24@wisc.edu>
2021-09-26 17:10:31 -0500

    Progress in expanding 8x8 tiles

--- CoordinateReshape_Zto2D.py
+++ CoordinateReshape_Zto2D.py
@@ -23,8 +23,10 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 sys.path.insert(0, pwd)
+import copy
 
 import athena_read as ath
+import h5py
 import hdf5_parser
 
 
@@ -32,6 +34,7 @@ import hdf5_parser
 
 #
 # data = ath.athdf(filename)
+rawhdf = h5py.File(filename, 'r')
 out = hdf5_parser.setup(filename)
 
 #%%
@@ -77,13 +80,21 @@ for b in B:
 ### New Z order to cartesian attempt
 
 #test with small array first
+# ZorderX = [0, 1, 0, 1, 2, 3, 2, 3, 0, 1, 0, 1, 2, 3, 2, 3]
+# ZorderY = [0, 0, 1, 1, 0, 0, 1, 1, 2, 2, 3, 3, 2, 2, 3, 3]
+# parsed array from hdf5_parser
 # ZorderX = out['posXarray']
 # ZorderY = out['posYarray']
-ZorderX = [0, 1, 0, 1, 2, 3, 2, 3, 0, 1, 0, 1, 2, 3, 2, 3]
-ZorderY = [0, 0, 1, 1, 0, 0, 1, 1, 2, 2, 3, 3, 2, 2, 3, 3]
+# raw hdf5 array
+ZorderX = []
+ZorderY = []
+for xyz in rawhdf['coordinates']:
+    ZorderX.append(xyz[0])
+    ZorderY.append(xyz[1])
+
 
 #setup large sequence
-xlen = int( np.sqrt(len(ZorderX)) )
+xlen = int( np.sqrt(len(ZorderX)) ) #ASSUME SQUARE BOX
 A = np.asarray(seqGenerator(xlen))
 B = np.multiply(2, A)
 twoD = []
@@ -94,7 +105,7 @@ for b in B:
     twoD.append(tempArray)
 print(len(twoD))
 
-#%%
+#assign Z indicies to coordinate array
 coords = np.empty((xlen, xlen), dtype=float)
 X = np.empty((xlen, xlen), dtype=float)
 Y = np.empty((xlen, xlen), dtype=float)
@@ -107,8 +118,47 @@ for y in range(0, len(twoD)): #rows
         X[y][x] = xpos
         Y[y][x] = ypos
 
+
+## Create 8x8 full resolution afterwards
+# stepX = X[0][1] - X[0][0] #array[Y][X]
+# stepY = Y[1][0] - Y[0][0] #array[Y][X]
+
+
+origX = copy.deepcopy(X)
+origY = copy.deepcopy(Y)
+stepX = origX[0][1] - origX[0][0]
+stepY = origY[1][0] - origY[0][0]
+# X = np.empty((xlen*8, xlen*8), dtype=float)
+# Y = np.empty((xlen*8, xlen*8), dtype=float)
+X = []
+Y = []
+# print("X step: " + str(stepX))
+# print("Y step: " + str(stepY))
+
+#arrayin - the 2D coordinate array you want to expand 8x8 each way
+#out - an empty 2D array
+# def expand (arrayin):
+arrayin = origX
+ylen = len(arrayin)
+xlen = len(arrayin[0])
+# out = np.empty((ylen, xlen), dtype=float)
+
+for row in range(0, len(origX)):
+    for col in range(0, len(origX[row])):
+        tempXlin = np.linspace(origX[row][col], origX[row][col] + stepX, 8)
+        tempYlin = np.linspace(origY[row][col], origY[row][col] + stepY, 8)
+        tempMeshgrid = np.meshgrid(tempXlin, tempYlin)
+        for row in range(0, len(tempMeshgrid[0])):
+            X.append(tempMeshgrid[0][row])
+            Y.append(tempMeshgrid[1][row])
+X, Y = (np.asarray(X), np.asarray(Y))
+# # print("Length of posX flattened: " + str(len(X.flatten())))
+# X = X.flatten()
+# Y = Y.flatten()
+        # tempXlin = np.linspace(coord[0], coord[0] + stepX, 8)
+        # out.append(np.asarray(tempMeshgrid[0]).flatten)
     
-#%%
+
 plt.clf()
 plt.plot(X, Y)
 # thingX = [X[5][5],  X[6][5], X[5][6], X[6][6]]
@@ -117,3 +167,7 @@ plt.plot(X, Y)
 # thingY = [Y[0][0],  Y[0][1], Y[1][0], Y[1][1]]
 # plt.plot(thingX, thingY)
 
+##RESULT? No good. Z order combined with existing hdf5_parser code
+##        mixed up 8x8 cartesian and z order arrays.
+#%%
+### New Z order to cartesian attempt
